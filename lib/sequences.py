b3d601989a58a72d605afa399f8e9cf29d7959ea
Jentrix <jentrix.ilusa@student.moringaschool.com>
2023-11-24 11:48:13 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = [0, 1]
+
+    while len(fibonacci_sequence) < length:
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_number)
+
+    print(f"Fibonacci sequence up to length {length}:")
+    print(fibonacci_sequence[:length])
+
+# Example: Print Fibonacci sequence up to length 9
+print_fibonacci(0)
+
+
